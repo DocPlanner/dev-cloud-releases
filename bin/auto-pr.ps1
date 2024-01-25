efef5a023f26edd7fb9e5f0152f849458c3f5e26
Daniel Garcia <danigpino@gmail.com>
2024-01-25 15:04:13 +0100

    dev-cloud manifest

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "DocPlanner/dev-cloud-releases:develop"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
