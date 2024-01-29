1706c6a3ff766474abc4cd8cae100ad8737a2c19
Daniel Garcia <danigpino@gmail.com>
2024-01-29 16:36:38 +0100

    fix style check

--- Casks/dev-cloud.rb
+++ Casks/dev-cloud.rb
@@ -1,14 +1,14 @@
 cask "aws-vault" do
     arch intel: "amd64"
-  
+
     version "2.17.0"
     sha256 intel: "2AD52B7EDCEE543B28E01559C5DCC04B47244CD8A39A212613C1EC6117053E2C"
-  
+
     url "https://github.com/DocPlanner/dev-cloud-releases/releases/download/#{version}/dev-#{version}-osx-x64.zip"
     name "Dev Cloud"
     desc "CLI to develop applications in the cloud"
     homepage "https://github.com/DocPlanner/dev-cloud"
-  
+
     binary "dev"
 
     depends_on formula: "awscli"
