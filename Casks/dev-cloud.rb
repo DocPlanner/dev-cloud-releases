0c883e48adbc04d6067bd1bd5fe2c4402706aa5f
Daniel Garcia <danigpino@gmail.com>
2024-02-01 10:38:19 +0100

    version latest

--- Casks/dev-cloud.rb
+++ Casks/dev-cloud.rb
@@ -1,6 +1,6 @@
 cask "dev-cloud" do
 
-    version "2.17.0"
+    version :latest
     sha256 :no_check
 
     url "https://github.com/DocPlanner/dev-cloud-releases/releases/download/#{version}/dev-#{version}-osx-x64.zip"
