ed17a365bb3a7b4e57c4d57de7c3d1383e00d6dd
Daniel Garcia Pino <danigpino@gmail.com>
2024-02-01 12:15:49 +0100

    Move require outside cask (#15)
    * move require outside cask
    
    * fix format

--- Casks/dev-cloud.rb
+++ Casks/dev-cloud.rb
@@ -1,25 +1,26 @@
+config_file = './local_config.rb'
+require config_file if File.file? config_file
+VENV_VERSION ||= '.'
+
 cask "dev-cloud" do
-    config_file = './local_config.rb'
-    require config_file if File.file? config_file
-    VENV_VERSION ||= '.'
 
-    version VENV_VERSION
-    sha256 :no_check
+  version VENV_VERSION
+  sha256 :no_check
 
-    url "https://github.com/DocPlanner/dev-cloud-releases/releases/download/#{version}/dev-#{version}-osx-x64.zip"
-    name "Dev Cloud"
-    desc "CLI to develop applications in the cloud"
-    homepage "https://github.com/DocPlanner/dev-cloud"
+  url "https://github.com/DocPlanner/dev-cloud-releases/releases/download/#{version}/dev-#{version}-osx-x64.zip"
+  name "Dev Cloud"
+  desc "CLI to develop applications in the cloud"
+  homepage "https://github.com/DocPlanner/dev-cloud"
 
-    binary "dev"
-    auto_updates true
+  binary "dev"
+  auto_updates true
 
-    livecheck do
-      url "https://github.com/DocPlanner/dev-cloud-releases"
-      strategy :github_latest
-    end
-
-    depends_on formula: "awscli"
-    depends_on formula: "devspace"
-    depends_on formula: "kubernetes-cli"
+  livecheck do
+    url "https://github.com/DocPlanner/dev-cloud-releases"
+    strategy :github_latest
   end
+
+  depends_on formula: "awscli"
+  depends_on formula: "devspace"
+  depends_on formula: "kubernetes-cli"
+end
